8092718145c6c4e7241823b6fd1a9a03c1ed4b88
Nazlican Yalcin <nazlican.yalcin@ericsson.com>
2019-01-14 20:18:13 +0300

    Initial commit

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+self.addEventListener('install',function(){
+console.log("SW Installed..");
+event.waitUntil(caches.open('static').then(function(cache){
+  cache.add('/');
+  cache.add('/index.html');
+  cache.add('/js/methods.js');
+  cache.add('/css/switch.css');
+  cache.add('/src/img/arrows.png');
+  cache.add('/src/img/black-banner.png');
+  cache.add('/src/img/ericsson_logo.png');
+  cache.add('/src/img/ericsson_sade.patt');
+  cache.add('/src/img/ericsson.patt');
+  cache.add('/src/img/ericsson.png');
+  cache.add('/src/img/humidity.png');
+  cache.add('/src/img/illuminance.png');
+  cache.add('/src/img/loudness.png');
+  cache.add('/src/img/temperature.png');
+  
+
+}));
+
+
+});
+
+self.addEventListener('activate',function(){
+console.log("SW Activate");
+});
